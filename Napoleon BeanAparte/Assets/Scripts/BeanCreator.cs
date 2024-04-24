8a5cfc53412cb25c6a85ca9dd3c896ebfc26efee
Gustje <gust.delogi@gmail.com>
2024-04-24 17:12:52 +0200

    Push Push Push
    Push

--- Napoleon BeanAparte/Assets/Scripts/BeanCreator.cs
+++ Napoleon BeanAparte/Assets/Scripts/BeanCreator.cs
@@ -1,18 +1,47 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PointSystem : MonoBehaviour
 {
+    [SerializeField]
+    private bool _orderComplete;
+    [SerializeField]
+    private int _beanCount = 20;
+    [SerializeField]
+    private GameObject[] _beans;
+    [SerializeField]
+    private GameObject _mud;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        SpawnMud();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(_orderComplete)
+        {
+            SpawnMud();
+        }
+
+    }
+
+    private void SpawnMud()
+    {
+        Instantiate(_mud, transform.position, Quaternion.identity);
+        SpawnBeans();
         
+    }
+
+    private void SpawnBeans()
+    {
+        for(int i=0; i< _beanCount; i++)
+        {
+            int x = Random.Range(0, _beanCount -1);
+            Instantiate(_beans[x], transform.position, Quaternion.identity);
+        }
     }
 }
