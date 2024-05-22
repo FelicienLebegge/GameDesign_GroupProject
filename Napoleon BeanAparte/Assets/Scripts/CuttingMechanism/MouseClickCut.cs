84de3ee2bb96ee3bf1353403bb74a5008f1c1ba1
Gustje <gust.delogi@gmail.com>
2024-05-22 17:45:55 +0200

    bean values
    BRUH

--- Napoleon BeanAparte/Assets/Scripts/CuttingMechanism/MouseClickCut.cs
+++ Napoleon BeanAparte/Assets/Scripts/CuttingMechanism/MouseClickCut.cs
@@ -14,7 +14,7 @@ public class MouseClickCut : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _text;
     [SerializeField] private TextMeshProUGUI _miss;
 
-    [SerializeField] private int _points = 18;
+    [SerializeField] private int _points = 10;
 
     [Header("Timers")]
     private float _timer = 0f;
@@ -52,65 +52,83 @@ public class MouseClickCut : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2)), out hit, 10, 1 << 2) && hit.transform.tag == "Bean")
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2)), out hit, 10, 1 << 2))
                 {
                     Debug.Log("Object can be cut");
 
                     GameObject victim = hit.collider.gameObject;
-                    if (victim.tag == "Bean")
+                    if (victim.CompareTag("Pea"))
                     {
-                        if (victim.name == "French bean")
-                        {
-                            GetPoints();
-                            Debug.Log("Double points GOLDEN BEAT");
-                        }
-                        AudioManager.instance.Play("BeanCut");
-                        _timer = 0f;
-                        GetPoints();
-                        _text.enabled = true;
-                        _isTimerActive = true;
-
-                        if (_timer >= 0.5f)
-                        {
-                            _text.enabled = false;
-                            _timer = 0;
-                        }
-                        if (angle == Angle.Up)
-                        {
-                            Cutter.Cut(victim, hit.point, Vector3.up);
-
-                        }
-                        else if (angle == Angle.Forward)
-                        {
-                            Cutter.Cut(victim, hit.point, Vector3.forward);
-
-                        }
-                        else if (angle == Angle.Right)
-                        {
-                            Cutter.Cut(victim, hit.point, Vector3.right);
-
-                        }
+                        KitchenStates.Score += 0.5f * _points;
                     }
-                }
-                else
-                {
-                    AudioManager.instance.Play("CutMiss");
-                    _missTimer = 0f;
-                    _miss.enabled = true;
-                    _isMissTimerActive = true;
-
-                    if (_missTimer >= 0.5f)
+                    if (victim.CompareTag("Navy"))
                     {
-                        _miss.enabled = false;
-                        _missTimer = 0;
+                        KitchenStates.Score += 1.5f * _points;
+                    }
+                    if (victim.CompareTag("French"))
+                    {
+                        KitchenStates.Score += 5 * _points;
+                    }
+                    if (victim.CompareTag("Fava"))
+                    {
+                        KitchenStates.Score += 2* _points;
+                    }
+                    if (victim.CompareTag("Anasazi"))
+                    {
+                        KitchenStates.Score += 3* _points;
+                    }
+                    
+
+  
+                    AudioManager.instance.Play("BeanCut");
+                    _timer = 0f;
+                    GetPoints();
+                    _text.enabled = true;
+                    _isTimerActive = true;
+
+                    if (_timer >= 0.5f)
+                    {
+                        _text.enabled = false;
+                        _timer = 0;
+                    }
+                    if (angle == Angle.Up)
+                    {
+                        Cutter.Cut(victim, hit.point, Vector3.up);
+
+                    }
+                    else if (angle == Angle.Forward)
+                    {
+                        Cutter.Cut(victim, hit.point, Vector3.forward);
+
+                    }
+                    else if (angle == Angle.Right)
+                    {
+                        Cutter.Cut(victim, hit.point, Vector3.right);
+
                     }
                 }
             }
+            else
+            {
+                AudioManager.instance.Play("CutMiss");
+                _missTimer = 0f;
+                _miss.enabled = true;
+                _isMissTimerActive = true;
+
+                if (_missTimer >= 0.5f)
+                {
+                    _miss.enabled = false;
+                    _missTimer = 0;
+                }
+                    
+            }
+                
         }
+            
     }
-    private void GetPoints()
-    {
-        KitchenStates.Score += _points;
-        Debug.Log("Added 20 points");
-    }
+        
+    
+    
+    
 }
+
