633270c063cf0e9df229787b207f3e2cb86cf02e
Gustje <gust.delogi@gmail.com>
2024-05-25 16:36:00 +0200

    Feedback beans needs more fixing
    feedback

--- Napoleon BeanAparte/Assets/Scripts/Bean.cs
+++ Napoleon BeanAparte/Assets/Scripts/Bean.cs
@@ -2,21 +2,29 @@ using System;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Bean : MonoBehaviour
 {
     [Header("Text")]
     [SerializeField]
-    private TextMeshProUGUI _text;
+    private GameObject TextGO;
+    [SerializeField] private Vector3 _textTransform;
+    [SerializeField]
+    private string _rarity;
+    
     
     [SerializeField]
     private float _moveSpeed = 3f;
     [SerializeField]
     private Vector3 _beanPosition = new(0, 1.6f, -4.5f);
     [SerializeField]
-
     private Vector4 _offset = new(2, 4, 6, 8);
     private bool _isBeanMoving;
+    private bool _needsBeanText;
+    private bool _destroyText = false;
+
 
 
     private bool _hasBeenAddedToList = false;
@@ -36,24 +44,42 @@ public class Bean : MonoBehaviour
 
 
     void Awake()
-    {
+    {   
         Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
         rigidbody.isKinematic = true; //don't use the rigid body yet. Will be enabled in the cooking station
 
         Collider beanCollider = gameObject.GetComponent<Collider>();
         beanCollider.isTrigger = true; //make sure the collider is a trigger when instaniated in the dirt
     }
+    
+
 
     private void Update()
     {
         switch (KitchenStates.KitchenState)
         {
             case KitchenStates.CookingStation.Washing:
-                if(_isBeanMoving)
+                if (_isBeanMoving)
                 {
                     MoveBean();
                 };
-                    break;
+                if (_needsBeanText)
+                {
+                    TextGO.GetComponentInChildren<TextMeshProUGUI>().text = _rarity;
+                    TextGO = Instantiate(TextGO, TextGO.transform.position, Quaternion.identity);
+                    _needsBeanText = false;
+
+                }
+                if (!_isBeanMoving && _destroyText)
+                {
+                    Debug.Log("help");
+                    TextGO.SetActive(false);
+                    Destroy(TextGO.gameObject);
+                    _destroyText = false;
+
+                }
+
+                break;
             case KitchenStates.CookingStation.Cutting:
 
                 if(KitchenStates.AreBeansWashed == true) //if the cutting station is activated and there are beans that are already washed, destroy the leftoverbeans
@@ -72,6 +98,8 @@ public class Bean : MonoBehaviour
         }
     }
 
+
+
     private void DestroyUnwashedBeans()
     {
         if(!_hasBeenAddedToList)
@@ -88,9 +116,11 @@ public class Bean : MonoBehaviour
             if (!_isBeanMoving) // Makes the sound effect only play once, which is nice
             {
                 _isBeanMoving = true;
+                
                 AudioManager.instance.Play("BeanPop");
                 KitchenStates.Score += BeanValue;
-                _text.enabled = true;
+
+                _needsBeanText = true;
             }
         }
     }
@@ -104,9 +134,9 @@ public class Bean : MonoBehaviour
         else
         {
             // Bean has reached the target position
-            _text.enabled = true;
+            TextGO.SetActive(true);
             _isBeanMoving = false;
-
+            _destroyText = true;
             AddBean();
             
         }
