9600076e9fdfcedef334f6aff8f4e21fed4f3c5c
MajidYasariDevine <majid.yasari@student.howest.be>
2024-05-26 16:28:53 +0200

    Reset name , speed , dirt on replay button

--- Napoleon BeanAparte/Assets/Scripts/MenuBehaviour.cs
+++ Napoleon BeanAparte/Assets/Scripts/MenuBehaviour.cs
@@ -30,6 +30,11 @@ public class MenuBehaviour : MonoBehaviour
 
     public void Menu()
     {
+        Name = null;
+        KitchenStates.Score = 0;
+        KitchenStates.SpeedMultiplier = 1;
+        Washer.IsDirtTooSMall = false;
         SceneManager.LoadScene(0);
     }
+
 }
