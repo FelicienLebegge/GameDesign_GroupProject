27db0f88edc9598294fe39d22597215b36129245
FelicienLebegge <felicien.lebegge@gmail.com>
2024-05-28 16:38:04 +0200

    Startscreen ui code

--- Napoleon BeanAparte/Assets/InputScript.cs
+++ Napoleon BeanAparte/Assets/InputScript.cs
@@ -9,12 +9,16 @@ public class InputScript : MonoBehaviour
     public static Button _OK;
     [SerializeField] private TextMeshProUGUI _playername;
     [SerializeField] private TextMeshProUGUI _placeholderInside;
+    [SerializeField] private TextMeshProUGUI _currentRecord;
+    [SerializeField] private TextMeshProUGUI _recordHolder;
 
     private void Start()
     {
         inputfield = transform.Find("input").GetComponent<TMP_InputField>();
         _playername.text = $"Enter name first !! ";
         _placeholderInside.text = "Enter your name...";
+        _currentRecord.text = "Current HighScore: " + PlayerPrefs.GetFloat("HighScore");
+        _recordHolder.text = "by: " + PlayerPrefs.GetString("HighScoreHolder");
     }
 
     public void EnteredName()
